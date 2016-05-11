26f28e26f9a840cc172513ed2ceed5284aed25c7
laznrbfe <laznrbfe@163.com>
2016-05-11 09:33:02 +0800

    do nothing

--- js/marry.js
+++ js/marry.js
@@ -3,6 +3,7 @@ import React, { Component, PropTypes } from 'react';
 import { View, StyleSheet, Text, } from 'react-native';
 
 import Logo from './svg/Logo';
+import Back from './svg/Back';
 
 export default class Marry extends Component {
 
@@ -10,6 +11,7 @@ export default class Marry extends Component {
 		return (
 			<View style={styles.container} >
 				<Logo />
+				<Back />
 			</View>
 		);
 	}
