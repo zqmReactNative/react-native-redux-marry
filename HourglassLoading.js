0c88f28f95bc1bfddec869ca80558d39f59c8741
laznrbfe <laznrbfe@163.com>
2016-05-23 09:01:30 +0800

    沙漏loading

---
+++ HourglassLoading.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { PixleRatio } from 'react-native';
+import Svg, {
+    Circle,
+    G,
+    Path,
+    Polygon,
+    Rect,
+} from 'react-native-svg';
+
+export default class HourglassLoading extends  Component {
+
+  render() {
+    return (
+      <Svg x="0" y="0" width="136.913" height="136.914" viewBox="0 0 136.913 136.914" >
+
+      </Svg>
+    );
+  }
+}
