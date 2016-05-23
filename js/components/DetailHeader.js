21b61a66413b7ddf3341a93db61bad56c094d296
laznrbfe <laznrbfe@163.com>
2016-05-23 09:02:22 +0800

    详情页头部视图

---
+++ js/components/DetailHeader.js
@@ -0,0 +1,16 @@
+// DetailHeader.js
+// 详情页头部视图
+
+import React, { Component } from "react";
+
+import { View, Text, Image, StyleSheet } from "react-native";
+
+
+export default class DetailHeader extends Component {
+  render() {
+    return (
+      <View style={{height:80, backgroundColor: '#ccc'}}>
+      </View>
+    );
+  }
+}
