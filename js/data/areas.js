fed63ed8a2beaf58ebc218f94e160529522f82c0
laznrbfe <laznrbfe@163.com>
2016-05-31 08:25:15 +0800

    区域

---
+++ js/data/areas.js
@@ -0,0 +1,13 @@
+export const Areas = [
+  {title:"全部",id:0},
+  {title:"江汉区",id:2701},
+  {title:"江岸区",id:2702},
+  {title:"硚口区",id:2703},
+  {title:"汉阳区",id:2707},
+  {title:"武昌区",id:2704},
+  {title:"洪山区",id:2705},
+  {title:"青山区",id:2706},
+  {title:"其他",id:2708}
+];
+export const defaultId = 0;
+export const defaultTitle = "全部地区";
