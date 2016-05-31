3f4d0ca0728735256d22e3eed54757d96bfb7035
laznrbfe <laznrbfe@163.com>
2016-05-31 08:12:01 +0800

    图库排序列表

---
+++ js/data/sortBys.js
@@ -0,0 +1,8 @@
+export const SortBys = [
+  {title:"默认",id:1},
+  {title:"更新日期",id:2},
+  {title:"收藏人气",id:3},
+  {title:"点赞数",id:4}
+];
+export const defaultId = 1;
+export const defaultTitle = "默认排序";
