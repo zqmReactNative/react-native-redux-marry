37877ab85013346052c7a98efe80b126654509c7
zhangqingming <laznrbfe@163.com>
2016-05-31 21:00:26 +0800

    FilterView动画效果

--- js/tabs/Merchant.js
+++ js/tabs/Merchant.js
@@ -2,7 +2,7 @@
 // 商家
 
 import React, { Component, PropTypes } from 'react';
-import { View, StyleSheet, Text, ListView, Dimensions, TouchableOpacity, Animated, Easing } from 'react-native';
+import { View, StyleSheet, Text, ListView, Dimensions, TouchableOpacity, TouchableHighlight, Animated, Easing } from 'react-native';
 
 import FaceIcon from '../svg/FaceIcon';
 import NavigatorHeader from '../common/NavigatorHeader';
@@ -26,31 +26,58 @@ export default class Merchant extends Component {
     });
     this.state = {
       dataSource: ds.cloneWithRows([{}, {}]),
-      fadeAnim: new Animated.Value(0), // init opacity 0
+      // transitionFromLeft
+      fadeAnim: new Animated.Value(-screenWidth), // init opacity 0
+      transitionFromRight: new Animated.Value(-screenWidth),
     };
   }
   componentDidMount() {
-    Animated.timing(          // Uses easing functions
-      this.state.fadeAnim,    // The value to drive
-      {toValue: 1},           // Configuration
-    ).start();                // Don't forget start!
+    // Animated.timing(       // Uses easing functions
+    //   this.state.fadeAnim, // The value to drive
+    //   {
+    //     toValue: 0,        // Target
+    //     duration: 2000,    // Configuration
+    //   },
+    // ).start();             // Don't forget start!
   }
   _onPressLeft = ()=>{
     this.setState({
       isShowFilterView: !this.state.isShowFilterView,
     });
+    this._showFilterView()
   }
   _onPressRight = ()=>{
     this.setState({
       isShowFilterView: !this.state.isShowFilterView,
     });
+    this._showFilterView()
   }
+  _showFilterView = ()=>{
+    
+    Animated.timing(       // Uses easing functions
+      this.state.fadeAnim, // The value to drive
+      {
+        toValue: 0,        // Target
+        duration: 300,    // Configuration
+      },
+    ).start();             // Don't forget start!
+  }
+  _hideFilterView = ()=>{
+    Animated.timing(       // Uses easing functions
+        this.state.fadeAnim, // The value to drive
+        {
+          toValue: -screenWidth,        // Target
+          duration: 300,    // Configuration
+        },
+      ).start();             // Don't forget start!
+  }
+
   _renderRow = (rowData)=>{
     return (<MerchantCell />);
   }
   render() {
     return (
-      <Animated.View style={[styles.container, {opacity: this.state.fadeAnim}]}>
+      <Animated.View style={[styles.container, ]}>
         <NavigatorHeader title={"商家"}/>
 
           <FilterHeaderView
@@ -67,16 +94,14 @@ export default class Merchant extends Component {
           dataSource={this.state.dataSource}
           renderRow={this._renderRow}
           />
-        { 
-          this.state.isShowFilterView? 
-            
-            <TouchableOpacity onPress={this._onPressLeft} style={{position: 'absolute', top: 64+40, bottom: 0, width: screenWidth, left: 0, right: 0, backgroundColor: 'rgba(55, 55, 55, 0.5)'}}>
-            </TouchableOpacity>
-            :
-            <TouchableOpacity onPress={this._onPressLeft} style={{position: 'absolute', top: 64+40, bottom: 0, left: -screenWidth, right: screenWidth, backgroundColor: 'rgba(230, 223, 224, 0.5)'}}>
-            </TouchableOpacity>
-            
-        }
+          
+        <Animated.View style={[styles.mask, {left: this.state.fadeAnim }]}>
+          <TouchableOpacity activeOpacity={1} onPress={this._onPressLeft} style={{position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, backgroundColor: 'green'}}>
+            <View>
+            </View>
+          </TouchableOpacity>
+        </Animated.View>
+        
 
       </Animated.View>
     );
@@ -88,5 +113,14 @@ const styles = StyleSheet.create({
     flex: 1,
     // backgroundColor: '#2D2E3F',
     backgroundColor: 'white',
+  },
+  mask: {
+    position: 'absolute',
+    // left: -screenWidth, 
+    // right: screenWidth,
+    top: 64+40, 
+    bottom: 0,
+    width: screenWidth,
+    backgroundColor: 'rgba(55, 55, 55, 0.5)',
   },
 });
