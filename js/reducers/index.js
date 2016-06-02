a4ae93778657a838493c0c2061c33d78ef0e85a5
zhangqingming <laznrbfe@163.com>
2016-06-02 21:04:58 +0800

    root reducer

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,3 +1,15 @@
 // index.js
 import { combineReducers } from 'redux';
 import home from './home';
+import album from './album';
+import merchant from './merchant';
+import albumDetail from './albumDetail';
+import merchantDetail from './merchantDetail';
+
+export default combineReducers({
+	home,
+	album,
+	merchant,
+	albumDetail,
+	merchantDetail,
+});
