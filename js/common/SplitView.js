647045fb71eca39a9b245d47629542080c836815
zhangqingming <laznrbfe@163.com>
2016-06-07 14:09:10 +0800

    注释

--- js/common/SplitView.js
+++ js/common/SplitView.js
@@ -1,8 +1,51 @@
 // SplitView.js
 // 两个ListView,左边控制右边显示
-/*
-__d("marry/views/Filters/FiltersCategory.js",function(e,t,n,r){Object.defineProperty(r,"__esModule",{value:!0});var i=t("react-native/Libraries/react-native/react-native.js"),o=babelHelpers.interopRequireDefault(i),a=t("marry/components/Link.js"),s=babelHelpers.interopRequireDefault(a),u=t("marry/components/LoopView.js"),l=babelHelpers.interopRequireDefault(u),c=t("lodash/index.js"),p=babelHelpers.interopRequireDefault(c),f=i.StyleSheet.create({container:{flexDirection:"row",backgroundColor:"#fff"},cateContainer:{flex:1,backgroundColor:"#f7f7f7"},itemContainer:{flex:1,backgroundColor:"#fff"},parent:{height:39},parentContainer:{justifyContent:"center",alignItems:"center"},text:{color:"#666",fontSize:13},textSelected:{color:"#ff5942"},activeContainer:{paddingLeft:12,borderLeftWidth:2.5,borderLeftColor:"transparent"},child:{height:39,marginLeft:6,borderBottomColor:"#ebebeb",borderBottomWidth:1/px},childContainer:{justifyContent:"center",alignItems:"center"},active:{backgroundColor:"#fff"},activeText:{color:"#ff5942"},activeTextContainer:{borderLeftColor:"#ff5942"}}),h=function(e){function t(){babelHelpers.classCallCheck(this,t);var e=babelHelpers.possibleConstructorReturn(this,Object.getPrototypeOf(t).apply(this,arguments));e._getChildren=function(t){var n=e._categories[t],r=n.children.slice();return n["default"]||r.unshift({id:n.id,title:"\u5168\u90e8"+n.title+"\u5546\u5bb6"}),e._ds.cloneWithRows(r)},e._category=function(t){e.setState({active:t,children:e._getChildren(t)})},e._select=function(t,n){e.setState({selected:n,children:e._getChildren(e.state.active)}),e.props.onChange(t,n)},e._renderParentRow=function(t,n){var r=n==e.state.active;return o["default"].createElement(s["default"],{style:[f.parent,r&&f.active],contentContainerStyle:f.parentContainer,underlayColor:"rgba(255, 255, 255, 0.5)",onPress:e._category.bind(e,n)},o["default"].createElement(i.View,{style:[f.activeContainer,r&&f.activeTextContainer]},o["default"].createElement(i.Text,{style:[f.text,r&&f.activeText]},t.title)))},e._renderChildRow=function(t){return o["default"].createElement(s["default"],{style:f.child,underlayColor:"rgba(255, 255, 255, 0.5)",contentContainerStyle:f.childContainer,onPress:e._select.bind(e,t.title,t.id)},o["default"].createElement(i.Text,{style:[f.text,e.state.selected==t.id&&f.textSelected]},t.title))};var n=p["default"].cloneDeep(e.props.data);e._ds=new i.ListView.DataSource({rowHasChanged:function(e,t){return e.id!==t.id}});var r=0;return p["default"].every(n,function(t,n){var i=t.children,o=t.id;return o===e.props.selected?(r=n,!1):p["default"].find(i,{id:e.props.selected})?(r=n,!1):!0}),e._categories=n,e.state={parent:n,children:e._getChildren(r),active:r,selected:e.props.selected},e}return babelHelpers.inherits(t,e),babelHelpers.createClass(t,[{key:"render",value:function(){return o["default"].createElement(i.View,babelHelpers["extends"]({},this.props,{style:f.container}),o["default"].createElement(l["default"],{style:f.cateContainer,data:this.state.parent,renderRow:this._renderParentRow}),o["default"].createElement(i.ListView,{style:f.itemContainer,dataSource:this.state.children,renderRow:this._renderChildRow,pageSize:8}))}}]),t}(i.Component);h.displayName="FiltersCategory",r["default"]=h}),
-*/
+
+// __d("marry/views/Filters/FiltersCategory.js",
+// 	function(e,t,n,r){
+// 		Object.defineProperty(r,"__esModule",{value:!0});
+// 		var i=t("react-native/Libraries/react-native/react-native.js"),
+// 		o=babelHelpers.interopRequireDefault(i),
+// 		a=t("marry/components/Link.js"),
+// 		s=babelHelpers.interopRequireDefault(a),
+// 		u=t("marry/components/LoopView.js"),
+// 		l=babelHelpers.interopRequireDefault(u),
+// 		c=t("lodash/index.js"),
+// 		p=babelHelpers.interopRequireDefault(c),
+// 		f=i.StyleSheet.create({}),
+// 		h=function(e){
+// 			function t(){
+// 				babelHelpers.classCallCheck(this,t);
+// 				var e=babelHelpers.possibleConstructorReturn(this,Object.getPrototypeOf(t).apply(this,arguments));
+// 				e._getChildren=function(t){
+// 					var n=e._categories[t],r=n.children.slice();
+// 					return n["default"]||r.unshift({id:n.id,title:"\u5168\u90e8"+n.title+"\u5546\u5bb6"}),
+// 					e._ds.cloneWithRows(r)
+// 				},
+// 				e._category=function(t){
+// 					e.setState({active:t,children:e._getChildren(t)})
+// 				},
+// 				e._select=function(t,n){
+// 					e.setState({selected:n,children:e._getChildren(e.state.active)}),
+// 					e.props.onChange(t,n)
+// 				},
+// 				e._renderParentRow=function(t,n){
+// 					var r=n==e.state.active;
+// 					return o["default"].createElement(s["default"],{style:[f.parent,r&&f.active],contentContainerStyle:f.parentContainer,underlayColor:"rgba(255, 255, 255, 0.5)",onPress:e._category.bind(e,n)},
+// 						     o["default"].createElement(i.View,{style:[f.activeContainer,r&&f.activeTextContainer]},
+// 						     	o["default"].createElement(i.Text,{style:[f.text,r&&f.activeText]},t.title)))
+// 				},
+// 				e._renderChildRow=function(t){
+// 					return o["default"].createElement(s["default"],{style:f.child,underlayColor:"rgba(255, 255, 255, 0.5)",contentContainerStyle:f.childContainer,onPress:e._select.bind(e,t.title,t.id)},
+// 						o["default"].createElement(i.Text,{style:[f.text,e.state.selected==t.id&&f.textSelected]},t.title))};
+// 					var n=p["default"].cloneDeep(e.props.data);
+// 					e._ds=new i.ListView.DataSource({rowHasChanged:function(e,t){return e.id!==t.id}});
+// 					var r=0;
+// 					return p["default"].every(n,function(t,n){
+// 						var i=t.children,o=t.id;return o===e.props.selected?(r=n,!1):p["default"].find(i,{id:e.props.selected})?(r=n,!1):!0}),
+// 					e._categories=n,e.state={parent:n,children:e._getChildren(r),active:r,selected:e.props.selected},e}
+// 					return babelHelpers.inherits(t,e),babelHelpers.createClass(t,[{key:"render",value:function(){return o["default"].createElement(i.View,babelHelpers["extends"]({},this.props,{style:f.container}),o["default"].createElement(l["default"],{style:f.cateContainer,data:this.state.parent,renderRow:this._renderParentRow}),o["default"].createElement(i.ListView,{style:f.itemContainer,dataSource:this.state.children,renderRow:this._renderChildRow,pageSize:8}))}}]),t}(i.Component);h.displayName="FiltersCategory",r["default"]=h}),
+
 import React, { Component, PropTypes } from 'react';
 import { Animated, View, StyleSheet, Text, ListView, Dimensions, TouchableOpacity, TouchableHighlight } from 'react-native';
 
@@ -79,10 +122,12 @@ export default class SplitView extends Component {
 	}
 }
 
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		flexDirection: 'row',
+		flexDirection:"row",
+		backgroundColor:"#fff"
 	},
 	maskButton: {
 		position: 'absolute',
@@ -96,4 +141,50 @@ const styles = StyleSheet.create({
 	rightContainer: {
 		width: screenWidth/2,
 	},
+	cateContainer: {
+		flex:1,
+		backgroundColor:"#f7f7f7"
+	},
+	itemContainer: {
+		flex:1,
+		backgroundColor:"#fff"
+	},
+	parent: {
+		height:39
+	},
+	parentContainer: {
+		justifyContent:"center",
+		alignItems:"center"
+	},
+	text: {
+		color:"#666",
+		fontSize:13
+	},
+	textSelected: {
+		color:"#ff5942"
+	},
+	activeContainer: {
+		paddingLeft:12,
+		borderLeftWidth:2.5,
+		borderLeftColor:"transparent"
+	},
+	child: {
+		height:39,
+		marginLeft:6,
+		borderBottomColor:"#ebebeb",
+		borderBottomWidth:1/px
+	},
+	childContainer: {
+		justifyContent:"center",
+		alignItems:"center"
+	},
+	active: {
+		backgroundColor:"#fff"
+	},
+	activeText: {
+		color:"#ff5942"
+	},
+	activeTextContainer: {
+		borderLeftColor:"#ff5942"
+	}
 });
