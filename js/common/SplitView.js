fbf4b25f4d89e770fdeea2f55aeb6ef43791303a
laznrbfe <laznrbfe@163.com>
2016-06-07 09:46:39 +0800

    0607am

--- js/common/SplitView.js
+++ js/common/SplitView.js
@@ -1,6 +1,8 @@
 // SplitView.js
 // 两个ListView,左边控制右边显示
-
+/*
+__d("marry/views/Filters/FiltersCategory.js",function(e,t,n,r){Object.defineProperty(r,"__esModule",{value:!0});var i=t("react-native/Libraries/react-native/react-native.js"),o=babelHelpers.interopRequireDefault(i),a=t("marry/components/Link.js"),s=babelHelpers.interopRequireDefault(a),u=t("marry/components/LoopView.js"),l=babelHelpers.interopRequireDefault(u),c=t("lodash/index.js"),p=babelHelpers.interopRequireDefault(c),f=i.StyleSheet.create({container:{flexDirection:"row",backgroundColor:"#fff"},cateContainer:{flex:1,backgroundColor:"#f7f7f7"},itemContainer:{flex:1,backgroundColor:"#fff"},parent:{height:39},parentContainer:{justifyContent:"center",alignItems:"center"},text:{color:"#666",fontSize:13},textSelected:{color:"#ff5942"},activeContainer:{paddingLeft:12,borderLeftWidth:2.5,borderLeftColor:"transparent"},child:{height:39,marginLeft:6,borderBottomColor:"#ebebeb",borderBottomWidth:1/px},childContainer:{justifyContent:"center",alignItems:"center"},active:{backgroundColor:"#fff"},activeText:{color:"#ff5942"},activeTextContainer:{borderLeftColor:"#ff5942"}}),h=function(e){function t(){babelHelpers.classCallCheck(this,t);var e=babelHelpers.possibleConstructorReturn(this,Object.getPrototypeOf(t).apply(this,arguments));e._getChildren=function(t){var n=e._categories[t],r=n.children.slice();return n["default"]||r.unshift({id:n.id,title:"\u5168\u90e8"+n.title+"\u5546\u5bb6"}),e._ds.cloneWithRows(r)},e._category=function(t){e.setState({active:t,children:e._getChildren(t)})},e._select=function(t,n){e.setState({selected:n,children:e._getChildren(e.state.active)}),e.props.onChange(t,n)},e._renderParentRow=function(t,n){var r=n==e.state.active;return o["default"].createElement(s["default"],{style:[f.parent,r&&f.active],contentContainerStyle:f.parentContainer,underlayColor:"rgba(255, 255, 255, 0.5)",onPress:e._category.bind(e,n)},o["default"].createElement(i.View,{style:[f.activeContainer,r&&f.activeTextContainer]},o["default"].createElement(i.Text,{style:[f.text,r&&f.activeText]},t.title)))},e._renderChildRow=function(t){return o["default"].createElement(s["default"],{style:f.child,underlayColor:"rgba(255, 255, 255, 0.5)",contentContainerStyle:f.childContainer,onPress:e._select.bind(e,t.title,t.id)},o["default"].createElement(i.Text,{style:[f.text,e.state.selected==t.id&&f.textSelected]},t.title))};var n=p["default"].cloneDeep(e.props.data);e._ds=new i.ListView.DataSource({rowHasChanged:function(e,t){return e.id!==t.id}});var r=0;return p["default"].every(n,function(t,n){var i=t.children,o=t.id;return o===e.props.selected?(r=n,!1):p["default"].find(i,{id:e.props.selected})?(r=n,!1):!0}),e._categories=n,e.state={parent:n,children:e._getChildren(r),active:r,selected:e.props.selected},e}return babelHelpers.inherits(t,e),babelHelpers.createClass(t,[{key:"render",value:function(){return o["default"].createElement(i.View,babelHelpers["extends"]({},this.props,{style:f.container}),o["default"].createElement(l["default"],{style:f.cateContainer,data:this.state.parent,renderRow:this._renderParentRow}),o["default"].createElement(i.ListView,{style:f.itemContainer,dataSource:this.state.children,renderRow:this._renderChildRow,pageSize:8}))}}]),t}(i.Component);h.displayName="FiltersCategory",r["default"]=h}),
+*/
 import React, { Component, PropTypes } from 'react';
 import { Animated, View, StyleSheet, Text, ListView, Dimensions, TouchableOpacity, TouchableHighlight } from 'react-native';
 
@@ -30,8 +32,20 @@ export default class SplitView extends Component {
 	}
 	_renderLeftListViewRow = (rowData)=>{
 		return (
-			<TouchableHighlight>
-				<View>
+			<TouchableHighlight style={{backgroundColor: '#f7f7f7'}} onPress={()=>alert(1)} underlayColor='#dbdbdb'>
+				<View style={{flexDirection: 'row', width: screenWidth/2, height: 40, alignItems: 'center', justifyContent: 'center'}}>
+					<View style={{width: 2, height: 15, backgroundColor: '#ff5942'}}>
+					</View>
+					<Text style={{marginLeft: 10, width: 80, fontSize: 15, color: '#666'}}>XXXX</Text>
+				</View>
+			</TouchableHighlight>
+		);
+	}
+	_renderRightListViewRow = (rowData)=>{
+		return (
+			<TouchableHighlight style={{backgroundColor: 'white'}} onPress={()=>alert(1)} underlayColor='#dbdbdb'>
+				<View style={{flexDirection: 'row', width: screenWidth/2, height: 40, alignItems: 'center', justifyContent: 'center'}}>
+					<Text style={{marginLeft: 10, fontSize: 15, color: '#666'}}>XXXX</Text>
 				</View>
 			</TouchableHighlight>
 		);
@@ -39,7 +53,7 @@ export default class SplitView extends Component {
 	render(){
 		const { style } = this.props;
 		return (
-			<TouchableOpacity style={[styles.maskButton, {flexDirection: 'row'}]} >
+			<TouchableHighlight style={[styles.maskButton]} onPress={()=>alert(0)}>
 				{/*
 
 				<View style={{styles.container}}>
@@ -47,18 +61,20 @@ export default class SplitView extends Component {
 					<ListView />
 				</View>
 				*/}
-				<ListView
-					style={styles.leftContainer}
-					dataSource={this.state.dataSourceOfLeft}
-					renderRow={(rowData)=><Text>LEFT</Text>}
-					/>
+				<View style={{flexDirection: 'row'}}>
+					<ListView
+						style={styles.leftContainer}
+						dataSource={this.state.dataSourceOfLeft}
+						renderRow={this._renderLeftListViewRow}
+						/>
 
-				<ListView
-					style={styles.rightContainer}
-					dataSource={this.state.dataSourceOfRight}
-					renderRow={(rowData)=><Text>RIGHT</Text>}
-					/>
-			</TouchableOpacity>
+					<ListView
+						style={styles.rightContainer}
+						dataSource={this.state.dataSourceOfRight}
+						renderRow={this._renderRightListViewRow}
+						/>
+				</View>
+			</TouchableHighlight>
 		);
 	}
 }
