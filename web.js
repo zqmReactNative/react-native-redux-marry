b97aa4a8a0315ec5ef50fbfafbe79d69d576a496
laznrbfe <laznrbfe@163.com>
2016-06-27 09:28:19 +0800

    0627am

--- web.js
+++ web.js
@@ -6,34 +6,68 @@ import ReactNative, {
   ListView,
   Text,
   Image,
+  TouchableOpacity,
+  Dimensions,
 } from 'react-native';
 
+import WebCell from './webcell';
+
+const kScreenWidth  = Dimensions.get('window').width;
+const kScreenHeight = Dimensions.get('window').height;
+
+
+const CellType = {
+  Img_Text_4 : "Img_Text_4",
+  Text_5 : "Text_5",
+  Text_GrayBg_4 : "Text_GrayBg_4",
+  Text_Blue_4 : "Text_Blue_4",
+  Img_Text_5 : "Img_Text_5"
+}
+
+const webs = [
+  {
+		title : "",
+		list:[
+			{img:"./Img/baidu.png", title:"百度", cellType: CellType.Img_Text_4},
+			{img:"./Img/baidu.png", title:"新浪", cellType: CellType.Img_Text_4},
+      {img:"./Img/baidu.png", title:"淘宝", cellType: CellType.Img_Text_4},
+			{img:"./Img/baidu", title:"8590", cellType: CellType.Img_Text_4},
+      {img:"./Img/baidu", title:"搜狐", cellType: CellType.Img_Text_4},
+			{img:"./Img/baidu", title:"微博", cellType: CellType.Img_Text_4},
+      {img:"./Img/baidu", title:"天猫", cellType: CellType.Img_Text_4},
+			{img:"./images/404", title:"腾讯", cellType: CellType.Img_Text_4},
+		],
+	},
+]
+
 export default class WebView extends Component {
+
+  _getRowData = (dataBlob, sectionID, rowID)=>{
+    return dataBlob[rowID];
+  }
+  _getSectionHeaderData = (dataBlob, sectionID)=>{
+    return dataBlob[sectionID];
+  }
+
   constructor(props) {
     super(props);
-    let _getRowData = (dataBlob, sectionID, rowID)=>{
-  		return dataBlob[rowID];
-		}
-		let _getSectionHeaderData = (dataBlob, sectionID)=>{
-  		return dataBlob[sectionID];
-		}
+
     let ds = new ListView.DataSource({
-      // getRowData: _getRowData,
-			// getSectionHeaderData: _getSectionHeaderData,
       rowHasChanged: (r1, r2)=> r1 !== r2,
-      // sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
     });
+
     this.state = {
-      dataSource: ds.cloneWithRows([{}, {}, {}, {}]),
+      dataSource: ds.cloneWithRows(webs[0].list),
     };
   }
-  _renderRow = (rowData, sectionID, rowID, highlightRow) => {
+  _renderRow = (rowData) => {
+    let img = rowData.img;
     return (
-      <View style={{width: 100, height: 40, backgroundColor: 'gray'}}>
-        <Text>
-          xxxx
-        </Text>
-      </View>
+      <WebCell
+        title={rowData.title}
+        /*source={{uri: rowData.img}}*/
+        source={require('./Img/baidu.png')}
+        />
     );
   }
   render() {
@@ -42,6 +76,8 @@ export default class WebView extends Component {
         <ListView
           style={styles.listView}
           renderRow={this._renderRow}
+          enableEmptySections={true}
+          // renderHeader={this._renderSectionHeader}
           dataSource={this.state.dataSource}
           contentContainerStyle={styles.contentContainerStyle}
           />
--- webcell.js
+++ webcell.js
@@ -1,16 +1,108 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactNative, {
   Navigator,
   View,
   Text,
-  Image
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
 } from 'react-native';
 
+
+const kScreenWidth  = Dimensions.get('window').width;
+const kScreenHeight = Dimensions.get('window').height;
+
+const CellType = {
+  Img_Text_4 : "Img_Text_4",
+  Text_5 : "Text_5",
+  Text_GrayBg_4 : "Text_GrayBg_4",
+  Text_Blue_4 : "Text_Blue_4",
+  Img_Text_5 : "Img_Text_5"
+}
+
 export default class WebView extends Component {
+  static propTypes = {
+    cellType: PropTypes.string,
+    onPress: PropTypes.func,
+    title: PropTypes.string,
+    detailTitle: PropTypes.string,
+    source: Image.propTypes.source,
+  }
+
+  static defaultProps = {
+    cellType: CellType.Text_5,
+  }
+
   render() {
+    let containerStyle = styles.container;
+    let buttonStyle = styles.buttonStyle;
+    let imageStyle = styles.imageLeftStyle;
+    let titleStyle = styles.titleStyle;
+    const { source, title } = this.props;
+    switch (this.props.cellType) {
+      case CellType.Img_Text_4:
+
+        break;
+      case CellType.Text_5:
+
+        break;
+      case CellType.Text_GrayBg_4:
+
+        break;
+      case CellType.Text_Blue_4:
+
+        break;
+      case CellType.Img_Text_5:
+
+        break;
+      default:
+
+    }
     return (
-      <View>
+      <View style={containerStyle}>
+        <TouchableOpacity style={buttonStyle}>
+          {source && <Image source={source} style={imageStyle}/>}
+          <Text style={titleStyle}>
+            {title}
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: kScreenWidth/4,
+    height: 40,
+    flexDirection: 'row',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    backgroundColor: '#e7e7e7',
+
+    // padding: 5,
+  },
+  imageLeftStyle: {
+    width: 20,
+    height: 20,
+    // paddingRight: 5,
+    marginRight: 5,
+  },
+  imageTopStyle: {},
+  titleStyle: {},
+  titleBlueStyle: {},
+  buttonStyle: {
+    // padding: 5,
+    // margin: 5,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'pink',
+    flexDirection: 'row',
+  },
+  buttonGrayBgStyle: {
+    padding: 5,
+    backgroundColor: 'gray',
+  },
+});
